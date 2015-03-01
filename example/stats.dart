1394d7d229f15a934bbde9b16951ce8f99504b53
Kenneth Endfinger <kaendfinger@gmail.com>
2015-03-01 09:41:21 -0500

    Lots of Epicness

--- example/stats.dart
+++ example/stats.dart
@@ -1,6 +1,8 @@
 import "package:osx/osx.dart";
 
 void main() {
+  say("Your computer is named: ${SystemInformation.getComputerName()}");
+  say("You are running OSX ${SystemInformation.getVersion()}");
   say("Battery is at ${Battery.getLevel()}%");
   say("There are ${Applications.list().length} apps installed");
 }
