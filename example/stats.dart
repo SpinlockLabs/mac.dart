e9f0ea977e1d8c4ced378df0dc0edb255adbf29e
Kenneth Endfinger <kaendfinger@gmail.com>
2015-03-01 09:45:24 -0500

    Awesomeness

--- example/stats.dart
+++ example/stats.dart
@@ -5,4 +5,23 @@ void main() {
   say("You are running OSX ${SystemInformation.getVersion()}");
   say("Battery is at ${Battery.getLevel()}%");
   say("There are ${Applications.list().length} apps installed");
+
+  if (Finder.getWindowCount() == 0) {
+    say("Finder is not open.");
+  } else {
+    say("Finder is open.");
+  }
+
+  var app = Applications.get("Textual 5");
+
+  if (app.isInstalled()) {
+    say("Textual 5 is installed");
+  } else {
+    say("Textual 5 is not installed.");
+  }
+
+  say("Opening Mission Control");
+  MissionControl.activate();
+  sleep(FIVE_SECONDS);
+  MissionControl.close();
 }
