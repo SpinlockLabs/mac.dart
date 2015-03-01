5a317608495dd7da02f0e64d5c46450f1acba1f1
Kenneth Endfinger <kaendfinger@gmail.com>
2015-03-01 12:44:32 -0500

    Epic Commander

--- example/commander.dart
+++ example/commander.dart
@@ -1,10 +1,23 @@
 import "package:osx/osx.dart";
 
 void main() {
-  var result = SpeechRecognizer.select([
-    "Good",
-    "Bad"
-  ], prompt: "How are you?");
+  ask();
+}
 
-  say("You said you are ${result}");
+void ask() {
+  var result = SpeechRecognizer.select([
+    "Mission Control",
+    "Google Chrome",
+    "Textual 5",
+    "What can I open?"
+  ], prompt: "What would you like to open?");
+
+  if (result == "What can I open?") {
+    say("Mission Control, Google Chrome, and Textual 5");
+    ask();
+  } else if (result == "Mission Control") {
+    MissionControl.activate();
+  } else {
+    Applications.get(result).launch();
+  }
 }
