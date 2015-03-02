60fab4ca4d7f8f36df51634062d16ce268289179
Kenneth Endfinger <kaendfinger@gmail.com>
2015-03-02 01:15:16 -0500

    Tweaks

--- example/demo.dart
+++ example/demo.dart
@@ -1,19 +1,21 @@
 import "package:osx/osx.dart";
 
+List<String> features = [
+  "User Interface Elements",
+  "Notification APIs",
+  "Text to Speech",
+  "Speech Recognition",
+  "System Information",
+  "Volume Controls",
+  "Battery Information",
+  "Finder Interaction",
+  "Application Interaction",
+  "Keyboard Input Simulation",
+  "Sleep and Wake"
+];
+
 void main() {
   say("Dart + OSX = Win");
   say("Here are my features:");
-  [
-    "User Interface Elements"
-    "Notification APIs",
-    "Text to Speech",
-    "Speech Recognition",
-    "System Information",
-    "Volume Controls",
-    "Battery Information",
-    "Finder Interaction",
-    "Application Interaction",
-    "Keyboard Input Simulation",
-    "Sleep and Wake"
-  ].forEach(say);
+  features.forEach(say);
 }
